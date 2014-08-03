80839263b92417b7943cd36567f8ca6175e3547a
Ruud van Asseldonk <dev@veniogames.com>
2014-08-03 20:03:39 +0200

    port Vector3 partially

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use vector3::{Vector3, dot};
+
+mod vector3;
+
 fn main() {
-    println!("Hello world!");
+    let a = Vector3 { x: 0.0, y: 2.0, z: 0.0 };
+    let b = a.normalised();
+    let d = dot(a, b);
+    println!("The vector has magnitude {}.", a.magnitude());
+    println!("The normalised y-component is {}.", b.y);
+    println!("Their dot product is {}.", d);
 }
---
+++ src/vector3.rs
@@ -0,0 +1,48 @@
+// Robigo Luculenta -- Proof of concept spectral path tracer in Rust
+// Copyright (C) 2014 Ruud van Asseldonk
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+pub struct Vector3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32
+}
+
+pub fn dot(a: Vector3, b: Vector3) -> f32 {
+    a.x * b.x + a.y * b.y + a.z * b.z
+}
+
+impl Vector3 {
+    pub fn magnitude_squared(self) -> f32 {
+        dot(self, self)
+    }
+
+    pub fn magnitude(self) -> f32 {
+        self.magnitude_squared().sqrt()
+    }
+
+    pub fn normalised(self) -> Vector3 {
+        let magnitude = self.magnitude();
+        if magnitude == 0.0 {
+            self
+        } else {
+            Vector3 {
+                x: self.x / magnitude,
+                y: self.y / magnitude,
+                z: self.z / magnitude
+            }
+        }
+    }
+}
