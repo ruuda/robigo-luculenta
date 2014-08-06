00aef7b1bafd41aea4a63b07a38804abe1fa35d7
Ruud van Asseldonk <dev@veniogames.com>
2014-08-06 18:13:35 +0200

    add Quaternion struct

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,8 @@
-use vector3::{Vector3, cross, dot};
+use quaternion::{Quaternion};
 
-mod vector3;
+mod quaternion;
 
 fn main() {
-    let a = Vector3::new(0.0, 2.0, 0.0);
-    let b = a.normalised();
-    let c = Vector3::new(1.0, 3.0, 1.0);
-    let d = Vector3::new(1.0, 0.0, 0.0);
-    let dot = dot(a, b);
-    let sum = a + c;
-    let diff = a - c;
-    let neg = -a;
-    let mul_right = a * 2.0;
-    let cross = cross(a, d);
-    let rotated = a.rotate_towards(d);
-    let reflection = c.reflect(d);
-    println!("The vector a has magnitude {}.", a.magnitude());
-    println!("The vector a normalised is {}.", b);
-    println!("Their dot product is {}.", dot);
-    println!("The sum a + c is {}.", sum);
-    println!("The difference a - c is {}.", diff);
-    println!("The negation of a is {}.", neg);
-    println!("The scalar product a * 2 is {}.", mul_right);
-    println!("The cross product of a and d is {}.", cross);
-    println!("The vector a rotated towards d is {}.", rotated);
-    println!("The vector c reflected in d is {}.", reflection);
+    let a = Quaternion { w: 0.0, x: 1.0, y: 1.0, z: 1.0 };
+    println!("The quaternion a is {}.", a);
 }
