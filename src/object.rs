ae002d6044a73f3b6c11cb8ec8a7241ff3aa06bf
Ruud van Asseldonk <dev@veniogames.com>
2014-08-13 10:19:40 +0200

    add Object

---
+++ src/object.rs
@@ -0,0 +1,24 @@
+// Robigo Luculenta -- Proof of concept spectral path tracer in Rust
+// Copyright (C) 2014 Ruud van Asseldonk
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+use geometry::Surface;
+use material::{Material, EmissiveMaterial};
+
+/// Represents a surface with a material.
+pub enum Object {
+    Reflective(Box<Surface>, Box<Material>),
+    Emissive(Box<Surface>, Box<EmissiveMaterial>)
+}
