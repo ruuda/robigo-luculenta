311337be63b198fc69acf96db6c394310a8d61d4
Ruud van Asseldonk <dev@veniogames.com>
2014-08-16 18:42:13 +0200

    add srgb::transform for CIE XYZ -> sRGB

--- src/srgb.rs
+++ src/srgb.rs
@@ -14,11 +14,28 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+use vector3::Vector3;
+
 /// Applies the sRGB gamma correction to the component.
 fn gamma_correct(f: f32) -> f32 {
     if f <= 0.0031308 {
         12.92 * f
     } else {
         1.055 * f.pow(1.0 / 2.4) - 0.055
+    }
+}
+
+/// Converts a CIE XYZ tristimulus to an sRGB colour.
+pub fn transform(cie: Vector3) -> Vector3 {
+    // Apply the sRGB matrix.
+    let r =  3.2406 * cie.x - 1.5372 * cie.y - 0.4986 * cie.z;
+    let g = -0.9689 * cie.x + 1.8758 * cie.y + 0.0415 * cie.z;
+    let b =  0.0557 * cie.x - 0.2040 * cie.y + 1.0570 * cie.z;
+
+    // Then do gamma correction.
+    Vector3 {
+        x: gamma_correct(r),
+        y: gamma_correct(g),
+        z: gamma_correct(b)
     }
 }
