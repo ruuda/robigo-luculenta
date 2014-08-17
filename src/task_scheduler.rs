a34cd5a280a916c985387678ca369af8b98bc76b
Ruud van Asseldonk <dev@veniogames.com>
2014-08-17 19:39:32 +0200

    implement TaskScheduler::new

--- src/task_scheduler.rs
+++ src/task_scheduler.rs
@@ -14,10 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+use std::cmp::max;
 use std::collections::RingBuf;
 use time::{Timespec, get_time};
 use gather_unit::GatherUnit;
 use plot_unit::PlotUnit;
+use scene::Scene;
 use tonemap_unit::TonemapUnit;
 use trace_unit::TraceUnit;
 
@@ -57,4 +59,47 @@ struct TaskScheduler<'s> {
     /// Whether a new gather task has been executed since the last
     /// tonemapping task was executed.
     image_changed: bool
+}
+
+impl<'s> TaskScheduler<'s> {
+    /// Creates a new task scheduler, that will render `scene` to a
+    /// canvas of the specified size, using `concurrency` threads.
+    pub fn new<'sc>(concurrency: uint,
+                    width: uint,
+                    height: uint,
+                    scene: &'sc Scene)
+                    -> TaskScheduler<'sc> {
+        // More trace units than threads seems sensible,
+        // but less plot units is acceptable,
+        // because one plot unit can handle multiple trace units.
+        let n_trace_units = concurrency * 3;
+        let n_plot_units = max(1u, concurrency / 2);
+
+        // Build the trace units.
+        let trace_units = range(0, n_trace_units)
+        .map(|_| { box TraceUnit::new(scene, width, height) })
+        .collect::<RingBuf<Box<TraceUnit>>>();
+
+        // Then build the plot units.
+        let plot_units = range(0, n_plot_units)
+        .map(|_| { box PlotUnit::new(width, height) })
+        .collect::<RingBuf<Box<PlotUnit>>>();
+
+        // There must be one gather unit and one tonemap unit.
+        let gather_unit = Some(box GatherUnit::new(width, height));
+        let tonemap_unit = Some(box TonemapUnit::new(width, height));
+
+        TaskScheduler {
+            number_of_trace_units: n_trace_units,
+            available_trace_units: trace_units,
+            done_trace_units: RingBuf::new(),
+            number_of_plot_units: n_plot_units,
+            available_plot_units: plot_units,
+            done_plot_units: RingBuf::new(),
+            gather_unit: gather_unit,
+            tonemap_unit: tonemap_unit,
+            last_tonemap_time: get_time(),
+            image_changed: false
+        }
+    }
 }
