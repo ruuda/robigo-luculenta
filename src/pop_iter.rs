947d3cdcd6cce05c06b88c59c2dfd2d11eb3749b
Ruud van Asseldonk <dev@veniogames.com>
2014-11-03 11:28:55 +0100

    fix and specialise pop_front_iter for libcollections reform
    This also removes `pop_iter`, which was not used. The implementation is
    not generic over the container any more, but specialised for `RingBuf`.
    In this case I can get away with it, because it is only used for
    `RingBuf`, but the generic approach was nicer in my opinion. Anyway,
    there will probably be more changes in libcollections, so let’s see what
    happens.

--- src/pop_iter.rs
+++ src/pop_iter.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-use std::collections::Deque;
+use std::collections::RingBuf;
 
 pub struct PopFrontItems<'a, C> where C: 'a {
     container: &'a mut C
@@ -24,8 +24,15 @@ pub trait PopFrontIter {
     fn pop_front_iter<'a>(&'a mut self) -> PopFrontItems<'a, Self>;
 }
 
-impl<'a, T, C> Iterator<T> for PopFrontItems<'a, C>
-    where C: PopFrontIter + Collection + Deque<T> {
+impl<T> PopFrontIter for RingBuf<T> {
+    fn pop_front_iter<'a>(&'a mut self) -> PopFrontItems<'a, RingBuf<T>> {
+        PopFrontItems {
+            container: self
+        }
+    }
+}
+
+impl<'a, T> Iterator<T> for PopFrontItems<'a, RingBuf<T>> {
     fn next(&mut self) -> Option<T> {
         self.container.pop_front()
     }
@@ -35,69 +42,12 @@ impl<'a, T, C> Iterator<T> for PopFrontItems<'a, C>
     }
 }
 
-impl<T, C> PopFrontIter for C where C: Collection + Deque<T> {
-    fn pop_front_iter<'a>(&'a mut self) -> PopFrontItems<'a, C> {
-        PopFrontItems {
-            container: self
-        }
-    }
-}
-
 #[test]
 fn pop_front_iter_ring_buf() {
-    use std::collections::RingBuf;
-
     let mut xs = RingBuf::new();
     xs.push(0u); xs.push(1); xs.push(2); xs.push(3); xs.push(4);
     let ys: Vec<uint> = xs.pop_front_iter().take(3).collect();
     assert_eq!(xs[0], 3u);
     assert_eq!(xs[1], 4u);
     assert_eq!(ys[], [0u, 1, 2][]);
-}
-
-pub struct PopItems<'a, C> where C: 'a {
-    container: &'a mut C
-}
-
-pub trait PopIter {
-    fn pop_iter<'a>(&'a mut self) -> PopItems<'a, Self>;
-}
-
-impl<'a, T, C> Iterator<T> for PopItems<'a, C>
-    where C: PopIter + Collection + MutableSeq<T> {
-    fn next(&mut self) -> Option<T> {
-        self.container.pop()
-    }
-
-    fn size_hint(&self) -> (uint, Option<uint>) {
-        (self.container.len(), Some(self.container.len()))
-    }
-}
-
-impl<T, C> PopIter for C where C: Collection + MutableSeq<T> {
-    fn pop_iter<'a>(&'a mut self) -> PopItems<'a, C> {
-        PopItems {
-            container: self
-        }
-    }
-}
-
-#[test]
-fn pop_iter_vec() {
-    let mut xs = vec!(0u, 1, 2, 3, 4);
-    let ys: Vec<uint> = xs.pop_iter().take(3).collect();
-    assert_eq!(xs[], [0u, 1][]);
-    assert_eq!(ys[], [4u, 3, 2][]);
-}
-
-#[test]
-fn pop_iter_ring_buf() {
-    use std::collections::RingBuf;
-
-    let mut xs = RingBuf::new();
-    xs.push(0u); xs.push(1); xs.push(2); xs.push(3); xs.push(4);
-    let ys: Vec<uint> = xs.pop_iter().take(3).collect();
-    assert_eq!(xs[0], 0u);
-    assert_eq!(xs[1], 1u);
-    assert_eq!(ys[], [4u, 3, 2][]);
 }
