385f266960d72c7515d99cd7dd947407c84b27e2
Ruud van Asseldonk <dev@veniogames.com>
2014-12-07 15:55:44 +0100

    do not explicitly allow if_let
    The feature has been added to the language, a directive is no longer necessary.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#![feature(if_let)]
 #![feature(slicing_syntax)]
 
 extern crate time;
